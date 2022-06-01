c5d5aa1bedf6762aabb2fc3c40a124d7b7c0fca5
FelingerGrisha <91113664+FelingerGrisha@users.noreply.github.com>
2022-06-01 20:10:26 +0700

    Add files via upload

---
+++ day20/part2.py
@@ -0,0 +1,24 @@
+import math
+
+def GetDivisors(n):
+    small_divisors = [i for i in range(1, int(math.sqrt(n)) + 1) if n % i == 0]
+    large_divisors = [n / d for d in small_divisors if n != d * d]
+    return small_divisors + large_divisors
+
+with open('input.txt', 'r') as iFile:
+    InputData = int(iFile.readline())
+
+lowestNumber = None
+i = 0
+
+while not lowestNumber:
+    i += 1
+    divisors = GetDivisors(i)
+
+    if not lowestNumber:
+        if sum(d for d in divisors if i / d <= 50) * 11 >= InputData:
+            lowestNumber = i
+            break
+
+with open('output2.txt', 'w') as oFile:
+    oFile.write(str(lowestNumber))
