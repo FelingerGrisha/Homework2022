17e6d1d2be8bbe14930e59425201f210a9260c81
FelingerGrisha <91113664+FelingerGrisha@users.noreply.github.com>
2022-06-01 12:00:14 +0700

    Add files via upload

---
+++ day12/part2.py
@@ -0,0 +1,23 @@
+import json
+
+with open('input.txt') as iFile:
+    InputData = json.load(iFile)
+
+def sumOfItem(item):
+
+    if isinstance(item, list):
+        return sum([sumOfItem(i) for i in item])
+
+    if isinstance(item, dict):
+        if 'red' in item.values():
+            return 0
+        return sum([sumOfItem(i) for i in item.values()])
+
+    if isinstance(item, str):
+        return 0
+
+    if isinstance(item, int):
+        return item
+
+with open('output2.txt', 'w') as oFile:
+    oFile.write(sumOfItem(InputData))
